ab9f52c58f996f5e76f6aabb428a3caa2fc1646c
John Kemp <stable.pseudonym@gmail.com>
2022-02-14 20:35:37 -0500

    minor format changes

--- test/kclient.go
+++ test/kclient.go
@@ -1,27 +1,55 @@
 package main
 
 import (
-	"log"
 	"context"
 	"fmt"
-	
+	"log"
+
 	"google.golang.org/grpc"
-	
+
 	keystonepb "github.com/regen-network/keystone2/keystone"
 )
 
+/*
+
+   Prototyping this to see if it fits...
+
+type keyring interface {
+
+	// Returns an Item matching the key or ErrKeyNotFound
+	Key(keyRef int32) (*KeyRef, error)
+
+	// Returns the non-secret parts of an Item
+	KeyMetadata(keyRef int32) (*KeyMetadata, error)
+
+	// Stores an Item on the keyring
+	NewKey(*KeySpec) error
+
+	// Removes the item with matching key
+	Remove(keyRef int32) error
+
+	// Signs input bytes with the referenced key
+	Sign(
+	// Provides a slice of all keys stored on the keyring
+	// by this entity
+	Keys() ([]*keystonepb.KeyRef, error)
+
+}
+
+*/
+
 func main() {
-	
+
 	fmt.Println("Keystone client ...")
 
 	opts := grpc.WithInsecure()
-	
+
 	cc, err := grpc.Dial("localhost:8080", opts)
-	
+
 	if err != nil {
 		log.Fatal(err)
 	}
-	
+
 	defer cc.Close()
 
 	client := keystonepb.NewKeyringClient(cc)
@@ -29,6 +57,7 @@ func main() {
 	request := &keystonepb.KeySpec{Label: "regen1fyccfg8ylh79ey2qdtx677k568mn0q3pnkajfk"}
 
 	resp, _ := client.NewKey(context.Background(), request)
-	
+
 	fmt.Printf("Receive response => [%v]", *resp.Label)
+
 }
