89c5d72a2104c2bc5dd2e2c6a1843c8c242b604e
John Kemp <stable.pseudonym@gmail.com>
2022-02-18 11:37:53 -0500

    initial types needed by all plugins

---
+++ plugin/types.go
@@ -0,0 +1,20 @@
+package types
+
+import (
+	pb "github.com/regen-network/keystone2/keystone"
+)
+
+// Options struct contains anything that is needed for configuring a
+// plugin with one element initially, a path to a configuration, which
+// can be interpreted differently by an individual plugin
+type Options struct {
+	ConfigPath string
+}
+
+// Plugin interface specifies the methods required for implementation
+// by a plugin
+type Plugin interface {
+	NewKey(in *pb.KeySpec, options Options) (*pb.KeyRef, error)
+	PubKey(in *pb.KeySpec, options Options) (*pb.PublicKey, error)
+	Sign(in *pb.Msg, options Options) (*pb.Signed, error)
+}
