01f24237def72cef71e9831e8ca3d5d6241079ac
John Kemp <unstable.pseudonym@stabledomain.net>
2022-05-15 15:01:10 -0400

    made the functions public - doh!

--- utils/utils.go
+++ utils/utils.go
@@ -7,7 +7,7 @@ import (
 )
 
 // randomBytes returns up to <size> crypto-random bytes
-func randomBytes(size int) (blk []byte, err error) {
+func RandomBytes(size int) (blk []byte, err error) {
     blk = make([]byte, size)
     _, err = rand.Read(blk)
     return
@@ -15,7 +15,7 @@ func randomBytes(size int) (blk []byte, err error) {
 
 // randomUint64 returns a crypto-random integer between 0 and the
 // maximum possible unsigned 64 bit integer.
-func randomUint64() (rnd uint64, err error) {
+func RandomUint64() (rnd uint64, err error) {
 	bigInt, err := rand.Int( rand.Reader, new(big.Int).SetUint64(math.MaxUint64) )
 
 	if err != nil {
